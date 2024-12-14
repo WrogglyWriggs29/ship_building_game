23160d88a2759fed51e403d6e44a9381a07e5d82
WrogglyWriggs29 <john.casey184@gmail.com>
2024-12-13 20:41:48 -0800

    added scene stack behavior to scenemanager

--- scripts/ship_design_scene/ship_blueprint_designer.gd
+++ scripts/ship_design_scene/ship_blueprint_designer.gd
@@ -191,6 +191,10 @@ func _process(_delta: float) -> void:
 		binder.deselect()
 
 func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		SceneStack.return_scene()
+		return
+
 	if event is InputEventMouseButton:
 		var mouse_event = event as InputEventMouseButton
 		if mouse_event.button_index == MOUSE_BUTTON_MIDDLE:
